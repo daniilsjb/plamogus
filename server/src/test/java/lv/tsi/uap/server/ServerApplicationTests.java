404cec5a640f6787f522c54f584991581bfefa01
daniilsjb <daniilb@outlook.com>
2023-04-25 23:22:55 +0300

    Add a local Spring profile and setup PostgreSQL to run in a Docker container

--- server/src/test/java/lv/tsi/uap/server/ServerApplicationTests.java
+++ server/src/test/java/lv/tsi/uap/server/ServerApplicationTests.java
@@ -1,13 +1,16 @@
 package lv.tsi.uap.server;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ServerApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
