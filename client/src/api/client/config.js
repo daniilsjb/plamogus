f59e1df23bdae22e82464b53a6fbb15f23c8b503
daniilsjb <daniilb@outlook.com>
2023-06-03 12:00:47 +0300

    Stage at AWS

--- client/src/api/client/config.js
+++ client/src/api/client/config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // TODO: Choose between local and production environments after deploying.
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
+    ? "http://localhost:8080/api/v1"
+    : "http://16.171.22.232:8080/api/v1",
 });
